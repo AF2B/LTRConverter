6ead961e0e5e6da3746b267ab76fa1818d1df538
andre.borba <dev_andre.filipe@outlook.com>
2023-10-17 22:31:15 -0300

    feat: main view

--- LTRConverter/ContentView.swift
+++ LTRConverter/ContentView.swift
@@ -8,14 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var leftAmount = ""
+    @State var rightAmount = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // backgroundView
+            Image("background")
+                .resizable()
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                // poneyImage
+                Image("prancingpony")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+                
+                // currency change text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                // conversion section
+                HStack {
+                    // left conversion
+                    VStack {
+                        // currency
+                        HStack {
+                            // curency image
+                            Image("silverpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 34)
+                            // currency text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                        }
+                        .padding(.bottom, -4)
+                        //text field
+                        TextField("Amount", text: $leftAmount)
+                            .padding(7)
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(7)
+                    }
+                    
+                    // equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundColor(.white)
+                    // right conversion
+                    VStack {
+                        // currency
+                        HStack {
+                            // currency text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundColor(.white)
+                            
+                            // curency image
+                            Image("goldpiece")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 34)
+                        }
+                        .padding(.bottom, -4)
+                        //text field
+                        TextField("Amount", text: $rightAmount)
+                            .padding(7)
+                            .background(Color(UIColor.systemGray6))
+                            .cornerRadius(7)
+                            .multilineTextAlignment(.trailing)
+                    }
+                }
+                .padding()
+                .background(.black.opacity(0.5))
+                .cornerRadius(52)
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    // info button
+                    Button {
+                        
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                    }
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                }
+                .padding(.trailing)
+            }
         }
-        .padding()
     }
 }
 
