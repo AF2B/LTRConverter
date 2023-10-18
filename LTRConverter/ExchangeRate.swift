7af8bc7241950aacb0fc9959dac02c6893465035
andre.borba <dev_andre.filipe@outlook.com>
2023-10-17 23:04:17 -0300

    feat: info view

---
+++ LTRConverter/ExchangeRate.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeRate.swift
+//  LTRConverter
+//
+//  Created by André Filipe Fonsêca Borba on 17/10/23.
+//
+
+import SwiftUI
+
+struct ExchangeRate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeRate()
+}
