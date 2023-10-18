53f5873d499536948e08e388897cb77720658fcc
andre.borba <dev_andre.filipe@outlook.com>
2023-10-17 21:23:04 -0300

    Initial Commit

---
+++ LTRConverter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LTRConverter
+//
+//  Created by André Filipe Fonsêca Borba on 17/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LTRConverter/LTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LTRConverterApp.swift
+//  LTRConverter
+//
+//  Created by André Filipe Fonsêca Borba on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct LTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
